5d699fa7dd87596453bbffe9443a616d08a40e19
Sudhanshu Singh <74963600+sudhanshusinghaiml@users.noreply.github.com>
2024-08-16 18:45:06 +0530

    Completed QA using prompt template

---
+++ QA_using_prompt_template.py
@@ -0,0 +1,41 @@
+""" This module contains the langchain integration with chainlit for developing a conversational application to  """
+
+import chainlit as cl
+import openai
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+from langchain.chains import LLMChain
+
+custom_template = \
+"""Question: {question}
+Answer: Let us think step by step."""
+
+@cl.on_chat_start
+def start_answering_prompt():
+    prompt_template = PromptTemplate(
+        template = custom_template,
+        input_variables = ["question"]
+    )
+
+    llm_chain = LLMChain(llm=OpenAI(temperature=0.5, streaming=True),verbose=True
+                         , prompt= prompt_template)
+
+    cl.user_session.set("llm_chain", llm_chain)
+
+
+
+@cl.on_message
+async def handle_message_main(message: cl.Message):
+    print(f"Received message: {message.content}")
+
+    llm_chain = cl.user_session.get("llm_chain")
+
+    # Prepare the input as a dictionary
+    input_message = {"question": message.content}
+
+    result = await llm_chain.acall(input_message, callbacks = [cl.AsyncLangchainCallbackHandler()])
+    
+    print(result)
+
+    await cl.Message(content=result["text"]).send()
